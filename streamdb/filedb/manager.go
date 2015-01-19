32752db0423b918d2280aad0868710e71a03a699
Daniel Kumor <daniel@dkumor.com>
2015-01-19 17:10:57 -0500

    Some Serious changes to filedb
    In order to make it work nicely with Kafka, it needs to be an
    honest-to-goodness database, rather than just files. Will be committing
    further code getting this thing working. Many ideas based on hbase.

--- streamdb/filedb/manager.go
+++ streamdb/filedb/manager.go
@@ -2,13 +2,26 @@ package filedb
 
 import (
     "os"
-    )
+    "path/filepath"
+)
 
 func PathExists(path string) bool {
     if _, err := os.Stat(path); err == nil {
         return true
     }
     return false
+}
+
+func MakeParentDirs(path string) (err error) {
+    //Check if the directory exists
+    parentdir := filepath.Dir(path)
+    if PathExists(parentdir) == false {
+        err = os.MkdirAll(parentdir,0777)
+        if (err != nil) {
+            return err
+        }
+    }
+    return nil
 }
 
 func DataSize(path string) (int64,error) {
