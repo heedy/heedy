a78f6a7f8b0159d6642a805b83ac48f0693ff634
Joseph <joehms22@gmail.com>
2015-02-10 14:35:36 -0500

    moved opening of databases to main streamdb gofile.

--- src/core/streamdb.go
+++ src/core/streamdb.go
@@ -8,11 +8,19 @@ import (
     "fmt"
     "streamdb/users"
     "os"
+    "plugins/web_client"
+    "streamdb/timebatchdb"
     )
 
 var (
     serverport = flag.Int("port", 8080, "The port number for the server to listen on.")
     helpflag = flag.Bool("help", false, "Prints this help message")
+
+    msgserver        = flag.String("msg", "localhost:4222", "The address of the messenger server")
+    mgoserver        = flag.String("mgo", "localhost", "The address of the MongoDB server")
+    mgodb            = flag.String("mgodb", "production_timebatchdb", "The name of the MongoDB database")
+    routes           = flag.String("route", ">", "The routes to write to database")
+
 )
 
 
@@ -26,10 +34,28 @@ func main() {
         return
     }
 
+    var err error
+    userdb, err := users.NewSqliteUserDatabase("production.sqlite")
+
+    if err != nil {
+        userdb = nil
+        log.Print("Cannot open user database")
+    }
+
+    go timebatchdb.DatabaseWriter(*msgserver, *mgoserver, *mgodb, *routes)
+    timedb, err := timebatchdb.OpenDatabase(*msgserver,*mgoserver,*mgodb)
+
+    if err != nil {
+        timedb = nil
+        log.Print("Cannot open timeseries database")
+
+    }
+
     log.Printf("Starting Server on port %d", *serverport)
 
     r := mux.NewRouter()
-    users.GetSubrouter(r)
+    users.GetSubrouter(userdb, timedb, r)
+    web_client.Setup(r, userdb)
     //r.HandleFunc("/", HomeHandler)
     http.Handle("/", r)
 
