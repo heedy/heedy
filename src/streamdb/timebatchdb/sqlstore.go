eecac0badc4c26fe2ca1adc6aa9ab3b8681220cf
Daniel Kumor <daniel@dkumor.com>
2015-03-01 19:06:51 -0500

    TimebatchDB: mongo->sql conversion
    The mongoDB storage method was removed from the database (if we ever need it, we can just resurrect it from git history).
    
    The database now uses SQLite3 for its storage mechanism. Once postgres support and config files are committed, an initializer for Postgres can be easily added.
    
    Benchmarks show rather poor performance of Sqlite in inserts of data - about 0.1s per insert. They show ~0.02 per get query. Hopefully this will not be the case with postgres.

---
+++ src/streamdb/timebatchdb/sqlstore.go
@@ -0,0 +1,246 @@
+package timebatchdb
+
+import (
+    "database/sql"
+    "errors"
+    )
+
+var (
+    ERROR_DATABASE_CORRUPTED = errors.New("Database is corrupted!")
+    ERROR_WTF = errors.New("Something is seriously wrong. A internal assertion failed.")
+    )
+
+//The DataRange which handles retrieving data from an Sql database
+type SqlRange struct {
+    r *sql.Rows
+    da *DatapointArray
+}
+
+//Clears all resources used by the SqlRange
+func (s *SqlRange) Close() {
+    if s.r!=nil {
+        s.r.Close()
+        s.r = nil
+    }
+}
+
+//A dummy function, it doesn't actually do anything.
+//It exists just so that SqlRange fits the DataRange interface
+func (s *SqlRange) Init() error {
+    return nil
+}
+
+//Returns the next datapoint from the SqlRange
+func (s *SqlRange) Next() (*Datapoint,error) {
+    d,_ := s.da.Next() //Next on DatapointArray never returns error
+    if d!=nil {
+        return d,nil
+    }
+
+    //The DatapointArray is now empty - check if the iterator is still functional
+    if s.r==nil {
+        return nil,nil
+    }
+
+    if !s.r.Next() {    //Check if there is more data to read
+        err := s.r.Err()
+        s.Close()
+        return nil,err
+    }
+
+    //There is more data to read!
+    var endindex uint64 //We don't actually care about this in our case - but we need to scan it
+    var data []byte
+    if err := s.r.Scan(&endindex,&data); err!=nil {
+        s.Close()
+        return nil,err
+    }
+
+    s.da = DatapointArrayFromCompressedBytes(data)
+
+    //Repeat the procedure.
+    return s.Next()
+}
+
+//The SqlStore stores and queries arrays of Datapoints in an SQL database. The table 'timebatchtable' is assumed
+//to already exist and the correct indices are assumed to already exist.
+type SqlStore struct {
+    inserter *sql.Stmt
+    timequery *sql.Stmt
+    indexquery *sql.Stmt
+    endindex *sql.Stmt
+}
+
+//Closes all resources associated with the SqlStore.
+func (s *SqlStore) Close() {
+    s.inserter.Close()
+    s.timequery.Close()
+    s.indexquery.Close()
+    s.endindex.Close()
+}
+
+//Returns the first index point outside of the most recent datapointarray stored within the database.
+//In effect, if the datapoints in a key were all in one huge array, returns array.length
+//(not including the datapoints which are not yet committed to the SqlStore)
+func (s *SqlStore) GetEndIndex(key string) (ei uint64,err error) {
+    rows,err := s.endindex.Query(key)
+    if err != nil {
+        return 0,err
+    }
+    if !rows.Next() {
+        return 0,ERROR_WTF //This should never happen
+    }
+    err=rows.Scan(&ei)
+    rows.Close()
+    return ei,err
+}
+
+//Inserts the given DatapointArray into the sql database given the startindex of the array for the key.
+func (s *SqlStore) Insert(key string, startindex uint64, da *DatapointArray) error {
+    _,err := s.inserter.Exec(key,da.Datapoints[da.Len()-1].Timestamp(),
+                startindex+uint64(da.Len()),da.CompressedBytes())
+    return err
+}
+
+//Appends the given DatapointArray to the data stream for key
+func (s *SqlStore) Append(key string, dp *DatapointArray) error {
+    i,err := s.GetEndIndex(key)
+    if (err!=nil) {
+        return err
+    }
+    return s.Insert(key,i,dp)
+}
+
+//Returns an SqlRange of datapoints starting at the starttime
+func (s *SqlStore) GetByTime(key string, starttime int64) (dr DataRange, startindex uint64, err error) {
+    rows, err := s.timequery.Query(key,starttime)
+    if err!=nil {
+        return EmptyRange{},0,err
+    }
+
+    if !rows.Next() {    //Check if there is any data to read
+        return EmptyRange{},0,rows.Err()
+    }
+
+    //There is some data!
+    var endindex uint64
+    var data []byte
+    if err = rows.Scan(&endindex,&data); err!=nil {
+        return EmptyRange{},endindex,err
+    }
+
+    da := DatapointArrayFromCompressedBytes(data).TStart(starttime)
+    if da==nil || uint64(da.Len()) > endindex {
+        rows.Close()
+        return EmptyRange{},endindex,ERROR_DATABASE_CORRUPTED
+    }
+
+    return &SqlRange{rows,da},endindex-uint64(da.Len()),nil
+}
+
+//Returns an SqlRange of datapoints starting at the nearest dataindex to the given startindex
+func (s *SqlStore) GetByIndex(key string, startindex uint64) (dr DataRange, dataindex uint64, err error) {
+    rows, err := s.indexquery.Query(key,startindex)
+    if err!=nil {
+        return EmptyRange{},0,err
+    }
+
+    if !rows.Next() {    //Check if there is any data to read
+        return EmptyRange{},0,rows.Err()
+    }
+
+    //There is some data!
+    var endindex uint64
+    var data []byte
+    if err = rows.Scan(&endindex,&data); err!=nil {
+        return EmptyRange{},endindex,err
+    }
+
+    da := DatapointArrayFromCompressedBytes(data)
+
+    if da==nil || uint64(da.Len()) > endindex {
+        rows.Close()
+        return EmptyRange{},endindex,ERROR_DATABASE_CORRUPTED
+    }
+
+    //Lastly, we start the DatapointArray from the correct index
+    //This subtraction is guaranteed to work on uint, since query requires $gt
+    fromend := endindex-startindex
+    if fromend < uint64(da.Len()) {
+        //The index we want is within the datarange
+        da = NewDatapointArray(da.Datapoints[da.Len()-int(fromend):])
+    }
+
+    return &SqlRange{rows,da},endindex-uint64(da.Len()),nil
+}
+
+
+//Initializes an sqlite database to work with an SqlStore.
+func OpenSQLiteStore(db *sql.DB) (*SqlStore,error) {
+    if err := db.Ping(); err != nil {
+        return nil,err
+    }
+    tx,err := db.Begin()
+    if err!=nil {
+        return nil, err
+    }
+
+    _,err = tx.Exec(`CREATE TABLE IF NOT EXISTS timebatchtable
+        (   Id INTEGER PRIMARY KEY,
+            Key STRING NOT NULL,
+            EndTime INTEGER,
+            EndIndex INTEGER,
+            Data BLOB
+            );`)
+    if err != nil {
+        tx.Rollback()
+        return nil, err
+    }
+
+    _,err = tx.Exec("CREATE INDEX IF NOT EXISTS KeyTime ON timebatchtable (Key,EndTime ASC)")
+    if err != nil {
+        tx.Rollback()
+        return nil,err
+    }
+
+    _,err = tx.Exec("CREATE INDEX IF NOT EXISTS KeyIndex ON timebatchtable (Key,EndIndex ASC)")
+    if err != nil {
+        tx.Rollback()
+        return nil,err
+    }
+
+    err = tx.Commit()
+    if err!=nil {
+        return nil,err
+    }
+
+    //Now that tables are all set up, prepare the queries to run on the database
+
+    inserter, err := db.Prepare("INSERT INTO timebatchtable VALUES (NULL,?,?,?,?);")
+    if err != nil {
+        return nil,err
+    }
+
+    timequery, err := db.Prepare("SELECT EndIndex,Data FROM timebatchtable WHERE Key=? AND EndTime > ? ORDER BY EndTime ASC")
+    if err != nil {
+        inserter.Close()
+        return nil,err
+    }
+
+    indexquery, err := db.Prepare("SELECT EndIndex,Data FROM timebatchtable WHERE Key=? AND EndIndex > ? ORDER BY EndIndex ASC")
+    if err != nil {
+        inserter.Close()
+        timequery.Close()
+        return nil,err
+    }
+
+    endindex, err := db.Prepare("SELECT ifnull(max(EndIndex),0) FROM timebatchtable WHERE Key=?")
+    if err != nil {
+        inserter.Close()
+        timequery.Close()
+        indexquery.Close()
+        return nil,err
+    }
+
+    return &SqlStore{inserter,timequery,indexquery,endindex},nil
+}
---
+++ src/streamdb/timebatchdb/sqlstore_test.go
@@ -0,0 +1,387 @@
+package timebatchdb
+
+import (
+    "testing"
+    "os"
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
+    )
+
+func SqlStoreTest(s *SqlStore, t *testing.T) {
+
+    //First check returning empties
+    i,err := s.GetEndIndex("hello/world")
+    if err!=nil || i!=0 {
+        t.Errorf("EndIndex of empty failed: %d %s",i,err)
+        return
+    }
+
+    r,si,err := s.GetByTime("hello/world",0)
+    if si!=0 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    if r.Init()!=nil {
+        t.Errorf("Error in DataRange init")
+        return
+    }
+    defer r.Close()
+    dp,err := r.Next()
+    if (dp!=nil || err!=nil) {
+        t.Errorf("Failed to get by time: %v",err)
+        return
+    }
+
+    r,si,err = s.GetByIndex("hello/world",0)
+    if si!=0 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    defer r.Close()
+    dp,err = r.Next()
+    if (dp!=nil || err!=nil) {
+        t.Errorf("Failed to get by index: %v",err)
+        return
+    }
+    dp,err = r.Next()
+    if (dp!=nil || err!=nil) {
+        t.Errorf("Failed to get by index: %v",err)
+        return
+    }
+
+    //Now insert some data
+    timestamps := []int64{1,2,3,4,5,6,6,7,8}
+    data := [][]byte{[]byte("test0"),[]byte("test1"),[]byte("test2"),[]byte("test3"),
+        []byte("test4"),[]byte("test5"),[]byte("test6"),[]byte("test7"),[]byte("test8")}
+
+    err = s.Append("hello/world",CreateDatapointArray(timestamps[0:1],data[0:1]))
+    if err != nil {
+        t.Errorf("Error in append: %s",err)
+        return
+    }
+
+    i,err = s.GetEndIndex("hello/world")
+    if err!=nil || i!=1 {
+        t.Errorf("EndIndex of nonempty failed: %d %s",i,err)
+        return
+    }
+    i,err = s.GetEndIndex("hello/world2")
+    if err!=nil || i!=0 {
+        t.Errorf("EndIndex of empty failed: %d %s",i,err)
+        return
+    }
+
+    //TIME
+
+    r,si,err = s.GetByTime("hello/world",0)
+    if r.Init()!=nil {
+        t.Errorf("Error in DataRange init")
+        return
+    }
+    defer r.Close()
+    if si!=0 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    dp,err = r.Next()
+    if (err!=nil || dp==nil || dp.Timestamp()!=1) {
+        t.Errorf("incorrect data for time: %v",err)
+        return
+    }
+    dp,err = r.Next()
+    if (err!=nil || dp!=nil) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+    dp,err = r.Next()
+    if (err!=nil || dp!=nil) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+
+    r,si,err = s.GetByTime("hello/world",1)
+    defer r.Close()
+    if si!=0 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    dp,err = r.Next()
+    if (err!=nil || dp!=nil) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+    dp,err = r.Next()
+    if (err!=nil || dp!=nil) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+
+    //INDEX
+
+    r,si,err = s.GetByIndex("hello/world",0)
+    defer r.Close()
+    if si!=0 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    dp,err = r.Next()
+    if (err!=nil || dp==nil || dp.Timestamp()!=1) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+    dp,err = r.Next()
+    if (err!=nil || dp!=nil) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+
+    r,si,err = s.GetByIndex("hello/world",1)
+    defer r.Close()
+    if si!=0 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    dp,err = r.Next()
+    if (err!=nil || dp!=nil || err!=nil) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+
+    err = s.Append("hello/world",CreateDatapointArray(timestamps[1:3],data[1:3]))
+    if err != nil {
+        t.Errorf("Error in append: %s",err)
+        return
+    }
+    i,err = s.GetEndIndex("hello/world")
+    if err!=nil || i!=3 {
+        t.Errorf("EndIndex of nonempty failed: %d %s",i,err)
+        return
+    }
+
+    r,si,err = s.GetByTime("hello/world",0)
+    defer r.Close()
+    if si!=0 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=1) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=2) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=3) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+    dp,err = r.Next()
+    if (dp!=nil || err!=nil) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+
+    r,si,err = s.GetByTime("hello/world",2)
+    defer r.Close()
+    if si!=2 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=3) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+    dp,err = r.Next()
+    if (dp!=nil || err!=nil) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+
+    r,si,err = s.GetByIndex("hello/world",0)
+    defer r.Close()
+    if si!=0 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=1) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=2) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=3) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+    dp,err = r.Next()
+    if (dp!=nil || err!=nil) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+
+    r,si,err = s.GetByIndex("hello/world",2)
+    if si!=2 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    defer r.Close()
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=3) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+    dp,err = r.Next()
+    if (dp!=nil || err!=nil) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+
+    err = s.Append("hello/world",CreateDatapointArray(timestamps[3:],data[3:]))
+    if err != nil {
+        t.Errorf("Error in append: %s",err)
+        return
+    }
+    i,err = s.GetEndIndex("hello/world")
+    if err!=nil || i!=9 {
+        t.Errorf("EndIndex of nonempty failed: %d %s",i,err)
+        return
+    }
+
+    r,si,err = s.GetByTime("hello/world",4)
+    if si!=4 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    defer r.Close()
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=5) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=6) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=6) {
+        t.Errorf("incorrect data for time")
+        return
+    }
+    r.Close()   //Test doulbe-closing
+
+    r,si,err = s.GetByIndex("hello/world",4)
+    defer r.Close()
+    if si!=4 || err!=nil {
+        t.Errorf("get failed %v %v",si,err)
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=5) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=6) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+    dp,err = r.Next()
+    if (dp==nil || err!=nil || dp.Timestamp()!=6) {
+        t.Errorf("incorrect data for index")
+        return
+    }
+    r.Close()
+}
+
+
+func TestSQLiteStore(t *testing.T) {
+    os.Remove("testing.db")
+    db,err := sql.Open("sqlite3","testing.db")
+    if err!=nil {
+        t.Errorf("Couldn't open database: %v",err)
+        return
+    }
+    defer db.Close()
+    s,err := OpenSQLiteStore(db)
+    if err!=nil {
+        t.Errorf("Couldn't create SQLiteStore: %v",err)
+        return
+    }
+    defer s.Close()
+
+    SqlStoreTest(s,t)
+
+}
+
+
+//This is a benchmark of how fast we can read out a thousand-datapoint range in chunks of 10 datapoints.
+func BenchmarkThousandSQLite(b *testing.B) {
+    os.Remove("testing.db")
+    db,err := sql.Open("sqlite3","testing.db")
+    if err!=nil {
+        b.Errorf("Couldn't open database: %v",err)
+        return
+    }
+    defer db.Close()
+    s,err := OpenSQLiteStore(db)
+    if err!=nil {
+        b.Errorf("Couldn't create SQLiteStore: %v",err)
+        return
+    }
+    defer s.Close()
+
+    data := [][]byte{[]byte("test0"),[]byte("test1"),[]byte("test2"),[]byte("test3"),
+        []byte("test4"),[]byte("test5"),[]byte("test6"),[]byte("test7"),[]byte("test8"),[]byte("test9")}
+    timestamps := []int64{0,1,2,3,4,5,6,7,8,9}
+    for i := int64(0);i<100;i++ {
+        s.Append("testkey",CreateDatapointArray(timestamps,data))
+    }
+    b.ResetTimer()
+    for n := 0; n < b.N; n++ {
+        r,_,_ := s.GetByIndex("testkey",0)
+
+        r.Init()
+        for dp,_ := r.Next(); dp!= nil ;dp,_ = r.Next() {
+            dp.Timestamp()
+            dp.Data()
+        }
+        r.Close()
+    }
+}
+
+func BenchmarkSQLiteInsert(b *testing.B) {
+    os.Remove("testing.db")
+    db,err := sql.Open("sqlite3","testing.db")
+    if err!=nil {
+        b.Errorf("Couldn't open database: %v",err)
+        return
+    }
+    defer db.Close()
+    s,err := OpenSQLiteStore(db)
+    if err!=nil {
+        b.Errorf("Couldn't create SQLiteStore: %v",err)
+        return
+    }
+    defer s.Close()
+
+    data := [][]byte{[]byte("test0"),[]byte("test1"),[]byte("test2"),[]byte("test3"),
+        []byte("test4"),[]byte("test5"),[]byte("test6"),[]byte("test7"),[]byte("test8"),[]byte("test9")}
+    timestamps := []int64{0,1,2,3,4,5,6,7,8,9}
+    b.ResetTimer()
+    for n := 0; n < b.N; n++ {
+        s.Append("testkey",CreateDatapointArray(timestamps,data))
+    }
+
+
+}
