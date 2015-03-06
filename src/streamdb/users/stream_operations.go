d3e9e084796eb23d8629d4c32e36a499301b17e4
Joseph <joehms22@gmail.com>
2015-03-06 17:51:54 -0500

    indentation fix in stream operations

--- src/streamdb/users/stream_operations.go
+++ src/streamdb/users/stream_operations.go
@@ -18,107 +18,111 @@ func (userdb *UserDatabase) CreateStream(Name, Type string, owner *Device) (int6
             OwnerId) VALUES (?,?,?);`,
             Name, Type, owner.Id)
 
-            if err != nil {
-                return 0, err
-            }
+    if err != nil {
+        return 0, err
+    }
 
-            return res.LastInsertId()
+    return res.LastInsertId()
+}
+
+// constructStreamsFromRows converts a rows statement to an array of streams
+func constructStreamsFromRows(rows *sql.Rows) ([]*Stream, error) {
+    out := []*Stream{}
+
+        // defensive programming
+        if rows == nil {
+            return out, ERR_INVALID_PTR
         }
 
-        // constructStreamsFromRows converts a rows statement to an array of streams
-        func constructStreamsFromRows(rows *sql.Rows) ([]*Stream, error) {
-            out := []*Stream{}
+        for rows.Next() {
+            u := new(Stream)
+            err := rows.Scan(
+                &u.Id,
+                &u.Name,
+                &u.Active,
+                &u.Public,
+                &u.Type,
+                &u.OwnerId,
+                &u.Ephemeral,
+                &u.Output)
 
-                // defensive programming
-                if rows == nil {
-                    return out, ERR_INVALID_PTR
-                }
+        out = append(out, u)
 
-                for rows.Next() {
-                    u := new(Stream)
-                    err := rows.Scan(
-                        &u.Id,
-                        &u.Name,
-                        &u.Active,
-                        &u.Public,
-                        &u.Type,
-                        &u.OwnerId,
-                        &u.Ephemeral,
-                        &u.Output)
+        if(err != nil) {
+            return out, err
+        }
+    }
 
-                        out = append(out, u)
-
-                        if(err != nil) {
-                            return out, err
-                        }
-                    }
-
-                    return out, nil
-                }
+    return out, nil
+}
 
 
-                // ReadStreamById fetches the stream with the given id and returns it, or nil if
-                // no such stream exists.
-                func (userdb *UserDatabase) ReadStreamById(id int64) (*Stream, error) {
-                    rows, err := userdb.db.Query("SELECT * FROM Stream WHERE Id = ? LIMIT 1", id)
+// ReadStreamById fetches the stream with the given id and returns it, or nil if
+// no such stream exists.
+func (userdb *UserDatabase) ReadStreamById(id int64) (*Stream, error) {
+    rows, err := userdb.db.Query("SELECT * FROM Stream WHERE Id = ? LIMIT 1", id)
 
-                    if err != nil {
-                        return nil, err
-                    }
+    if err != nil {
+        return nil, err
+    }
 
-                    defer rows.Close()
+    defer rows.Close()
 
-                    streams, err := constructStreamsFromRows(rows)
+    streams, err := constructStreamsFromRows(rows)
 
-                    if(len(streams) != 1) {
-                        return nil, errors.New("Wrong number of streams returned")
-                    }
+    if(len(streams) != 1) {
+        return nil, errors.New("Wrong number of streams returned")
+    }
 
-                    return streams[0], nil
-                }
+    return streams[0], nil
+}
 
-                func (userdb *UserDatabase) ReadStreamsByDevice(device *Device) ([]*Stream, error) {
-                    rows, err := userdb.db.Query("SELECT * FROM Stream WHERE OwnerId = ? LIMIT 1", device.Id)
+func (userdb *UserDatabase) ReadStreamsByDevice(device *Device) ([]*Stream, error) {
+    if device == nil {
+        return nil, ERR_INVALID_PTR
+    }
 
-                    if err != nil {
-                        return nil, err
-                    }
+    rows, err := userdb.db.Query("SELECT * FROM Stream WHERE OwnerId = ?", device.Id)
 
-                    return constructStreamsFromRows(rows)
-                }
+    if err != nil {
+        return nil, err
+    }
 
-                // UpdateStream updates the stream with the given ID with the provided data
-                // replacing all prior contents.
-                func (userdb *UserDatabase) UpdateStream(stream *Stream) (error) {
-                    if stream == nil {
-                        return ERR_INVALID_PTR
-                    }
+    return constructStreamsFromRows(rows)
+}
 
-
-                    _, err := userdb.db.Exec(`UPDATE Stream SET
-                        Name = ?,
-                        Active = ?,
-                        Public = ?,
-                        Type = ?,
-                        OwnerId = ?,
-                        Ephemeral = ?,
-                        Output = ?
-                        WHERE Id = ?;`,
-                        stream.Name,
-                        stream.Active,
-                        stream.Public,
-                        stream.Type,
-                        stream.OwnerId,
-                        stream.Ephemeral,
-                        stream.Output,
-                        stream.Id)
-
-                        return err
-                    }
+// UpdateStream updates the stream with the given ID with the provided data
+// replacing all prior contents.
+func (userdb *UserDatabase) UpdateStream(stream *Stream) (error) {
+    if stream == nil {
+        return ERR_INVALID_PTR
+    }
 
 
-                    // DeleteStream removes a stream from the database
-                    func (userdb *UserDatabase) DeleteStream(Id int64) (error) {
-                        _, err := userdb.db.Exec(`DELETE FROM Stream WHERE Id = ?;`, Id );
-                        return err
-                    }
+    _, err := userdb.db.Exec(`UPDATE Stream SET
+        Name = ?,
+        Active = ?,
+        Public = ?,
+        Type = ?,
+        OwnerId = ?,
+        Ephemeral = ?,
+        Output = ?
+        WHERE Id = ?;`,
+        stream.Name,
+        stream.Active,
+        stream.Public,
+        stream.Type,
+        stream.OwnerId,
+        stream.Ephemeral,
+        stream.Output,
+        stream.Id)
+
+    return err
+}
+
+
+// DeleteStream removes a stream from the database
+func (userdb *UserDatabase) DeleteStream(Id int64) (error) {
+    _, err := userdb.db.Exec(`DELETE FROM Stream WHERE Id = ?;`, Id );
+    return err
+}
