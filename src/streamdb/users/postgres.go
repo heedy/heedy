6063caff1363850ca4380e0dc98f622f662c3520
Joseph <joehms22@gmail.com>
2015-04-04 22:41:37 -0400

    Removed the unused postgres file in users

--- src/streamdb/users/postgres.go
+++
@@ -1,9 +0,0 @@
-package users
-
-/** Package users provides an API for managing user information.
-
-Copyright 2015 - Joseph Lewis <joseph@josephlewis.net>
-                 Daniel Kumor <rdkumor@gmail.com>
-
-All Rights Reserved
-**/
