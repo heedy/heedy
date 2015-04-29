a08090c72440cb27c78af2e9063a9925af34e3bd
Joseph <joehms22@gmail.com>
2015-04-28 20:14:30 -0400

    upgraded database_types_test to use the new testing framework

--- src/streamdb/users/database_types_test.go
+++ src/streamdb/users/database_types_test.go
@@ -1,8 +1,11 @@
 // Package users provides an API for managing user information.
 package users
 
-import "testing"
-
+import (
+	"testing"
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/require"
+)
 func TestSetNewPassword(t *testing.T) {
 	var j User
 	var k User
@@ -13,23 +16,11 @@ func TestSetNewPassword(t *testing.T) {
 	l.PasswordSalt = "tmp"
 	l.SetNewPassword("password")
 
-	if j == k {
-		t.Errorf("Setting password failed: %v vs %v", j, k)
-		return
-	}
-
-	if k == l {
-		t.Errorf("Salting Failed")
-		return
-	}
+	assert.NotEqual(t, j, k, "Setting password failed: %v vs %v", j, k)
+	assert.NotEqual(t, k, l, "salting failed")
 
 	j.SetNewPassword("password")
-
-	if k == j {
-		t.Errorf("Second Set Failed")
-		return
-
-	}
+	assert.NotEqual(t, k, j, "second set failed, same hash")
 }
 
 func TestAdmin(t *testing.T) {
@@ -38,15 +29,8 @@ func TestAdmin(t *testing.T) {
 	var k User
 	k.Admin = false
 
-	if k.IsAdmin() == true {
-		t.Errorf("False positive admin")
-		return
-	}
-
-	if j.IsAdmin() == false {
-		t.Errorf("False negative admin")
-		return
-	}
+	assert.NotEqual(t, k.IsAdmin(), true, "false positive admin")
+	assert.NotEqual(t, j.IsAdmin(), false, "false negative admin")
 }
 
 
@@ -95,315 +79,134 @@ func TestDevicePermissions(t *testing.T) {
 	var disabledSuper Device
 	disabledSuper.IsAdmin = true
 
-	if none.IsActive() {
-		t.Errorf("improper active check.")
-	}
-
-	if !onlyEnabled.IsActive() {
-		t.Errorf("improper active check.")
-	}
-
-	if onlyEnabled.IsAdmin {
-		t.Errorf("improper elevation of privliges.")
-	}
-
-	if !all.IsAdmin {
-		t.Errorf("Correct admin was denied")
-	}
+	assert.False(t, none.IsActive(), "improper active check")
+	assert.True(t, onlyEnabled.IsActive(), "improper active check")
+	assert.False(t, onlyEnabled.IsAdmin, "improper elevation of privliges")
+	assert.True(t, all.IsAdmin, "correct admin was denied")
 
 	// WriteAllowed
-
-	if none.WriteAllowed() {
-		t.Errorf("Granted write to unprivliged")
-	}
-
-	if !all.WriteAllowed() {
-		t.Errorf("Denied write to privliged device")
-	}
+	assert.False(t, none.WriteAllowed(), "granted write to unprivliged")
+	assert.True(t, all.WriteAllowed(), "denied write to privliged")
 
 	// WriteAnywhereAllowed
-
-	if none.WriteAnywhereAllowed() {
-		t.Errorf("Granted WriteAnywhereAllowed to unprivliged")
-	}
-
-	if !all.WriteAnywhereAllowed() {
-		t.Errorf("Denied WriteAnywhereAllowed to privliged device")
-	}
+	assert.False(t, none.WriteAnywhereAllowed(), "Granted WriteAnywhereAllowed to unprivliged")
+	assert.True(t, all.WriteAnywhereAllowed(), "Denied WriteAnywhereAllowed to privliged device")
 
 	// CanModifyUser
-
-	if none.CanActAsUser {
-		t.Errorf("Granted CanModifyUser to unprivliged")
-	}
+	assert.False(t, none.CanActAsUser)
 }
 
 func TestGte(t *testing.T) {
-	if ! NOBODY.Gte(ROOT) {
-		t.Errorf("nobody should be gt root")
-	}
-
-	if ! ROOT.Gte(USER) {
-		t.Errorf("root should be gt user")
-	}
-
-	if ! USER.Gte(DEVICE) {
-		t.Errorf("user should be gt device")
-	}
-
-	if ! DEVICE.Gte(FAMILY) {
-		t.Errorf("device should be gt family")
-	}
-
-	if ! FAMILY.Gte(ENABLED) {
-		t.Errorf("family should be gt enabled")
-	}
-
-	if ! ENABLED.Gte(ANYBODY) {
-		t.Errorf("enabled should be gt anybody")
-	}
-
-	if ! NOBODY.Gte(NOBODY) {
-		t.Errorf("nobody should equal nobody")
-	}
+	assert.True(t, NOBODY.Gte(ROOT))
+	assert.True(t, ROOT.Gte(USER))
+	assert.True(t, USER.Gte(DEVICE))
+	assert.True(t, DEVICE.Gte(FAMILY))
+	assert.True(t, FAMILY.Gte(ENABLED))
+	assert.True(t, ENABLED.Gte(ANYBODY))
+	assert.True(t, NOBODY.Gte(NOBODY))
 }
 
 func TestGeneralPermissions(t *testing.T) {
 	var d1 Device
 	d1.IsAdmin = true
 	d1.Enabled = true
+	assert.Equal(t, d1.GeneralPermissions(), ROOT)
+
 	var d2 Device
 	d2.Enabled = false
+	assert.Equal(t, d2.GeneralPermissions(), ANYBODY)
+
 	var d3 Device
 	d3.IsAdmin = true
 	d3.Enabled = false
+	assert.Equal(t, d3.GeneralPermissions(), ANYBODY)
+
 	var d4 Device
 	d4.Enabled = true
-
-
-	if d2.GeneralPermissions() != ANYBODY {
-		t.Errorf("disabled devices should be anybody")
-	}
-
-	if d1.GeneralPermissions() != ROOT {
-		t.Errorf("Enabled admin devices should be root")
-	}
-
-	if d3.GeneralPermissions() != ANYBODY {
-		t.Errorf("disabled admins should be anybody")
-	}
-
-	if d4.GeneralPermissions() != ENABLED {
-		t.Errorf("enabled non root should be enabled")
-	}
+	assert.Equal(t, d4.GeneralPermissions(), ENABLED)
 }
 
 func TestRelationToUser(t *testing.T) {
 	u, dev, _, err := CreateUDS(testdb)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.Nil(t, err)
 
-	if dev.RelationToUser(nil) != ANYBODY {
-		t.Errorf("nil user should be anybody")
-	}
+	assert.Equal(t, dev.RelationToUser(nil), ANYBODY, "nil user should be anybody")
 
 	dev.Enabled = false
-	if dev.RelationToUser(u) != ANYBODY {
-		t.Errorf("disabled devices should be anybody")
-	}
+	assert.Equal(t, dev.RelationToUser(u), ANYBODY, "disabled should be anybody")
 	dev.Enabled = true
 
 	dev.IsAdmin = true
-	if dev.RelationToUser(u) != ROOT {
-		t.Errorf("admin devices should have root")
-	}
+	assert.Equal(t, dev.RelationToUser(u), ROOT, "admin should have root")
 	dev.IsAdmin = false
 
 	dev.CanActAsUser = true
-	if dev.RelationToUser(u) != USER {
-		t.Errorf("devices with same userid should be users")
-	}
+	assert.Equal(t, dev.RelationToUser(u), USER, "devices with same userid should be users")
 	dev.CanActAsUser = false
 
-	if dev.RelationToUser(u) != DEVICE {
-		t.Errorf("devices under a user should be a device")
-	}
+	assert.Equal(t, dev.RelationToUser(u), DEVICE, "devices under a user should be a device")
 
 	dev.UserId = -1
-	if dev.RelationToUser(u) != ANYBODY {
-		t.Errorf("unrelated devices should be anybody")
-	}
+	assert.Equal(t, dev.RelationToUser(u),  ANYBODY, "unrelated devices should be anybody")
 }
-
-
 
 func TestRelationToDevice(t *testing.T) {
 	u, dev, _, err := CreateUDS(testdb)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.Nil(t, err)
 
 	d2, err := CreateTestDevice(testdb, u)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.Nil(t, err)
 
-
-	if dev.RelationToDevice(nil) != ANYBODY {
-		t.Errorf("nil dev should be anybody")
-	}
+	assert.Equal(t, dev.RelationToDevice(nil), ANYBODY, "nil dev should be anybody")
 
 	dev.Enabled = false
-	if dev.RelationToDevice(d2) != ANYBODY {
-		t.Errorf("disabled devices should be anybody")
-	}
+	assert.Equal(t, dev.RelationToDevice(d2), ANYBODY, "disabled devices should be anybody")
 	dev.Enabled = true
 
 	dev.IsAdmin = true
-	if dev.RelationToDevice(d2) != ROOT {
-		t.Errorf("admin devices should have root")
-	}
+	assert.Equal(t, dev.RelationToDevice(d2), ROOT, "admin devices should have root")
 	dev.IsAdmin = false
 
 	dev.CanActAsUser = true
-	if dev.RelationToDevice(d2) != USER {
-		t.Errorf("devices with same userid should be users")
-	}
+	assert.Equal(t, dev.RelationToDevice(d2), USER, "devices with same userid should be users")
 	dev.CanActAsUser = false
 
-	if dev.RelationToDevice(d2) != FAMILY {
-		t.Errorf("devices under a user should be a family")
-	}
-
-	if dev.RelationToDevice(dev) != DEVICE {
-		t.Errorf("Devices should be device with themselves")
-	}
+	assert.Equal(t, dev.RelationToDevice(d2),   FAMILY, "devices under a user should be a family")
+	assert.Equal(t, dev.RelationToDevice(dev),  DEVICE, "Devices should be device with themselves")
 
 	dev.UserId = -1
-	if dev.RelationToDevice(d2) != ENABLED {
-		t.Errorf("unrelated devices should be enabled")
-	}
+	assert.Equal(t, dev.RelationToDevice(d2), ENABLED, "unrelated devices should be enabled")
 }
 
 func TestRelationToStream(t *testing.T) {
 	_, dev, stream, err := CreateUDS(testdb)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.Nil(t, err)
 
-	if dev.RelationToStream(nil, dev) != ANYBODY {
-		t.Errorf("nil stream")
-	}
-
-	if dev.RelationToStream(stream, nil) != ANYBODY {
-		t.Errorf("nil parent")
-	}
+	assert.Equal(t, dev.RelationToStream(nil, dev), ANYBODY, "nil stream")
+	assert.Equal(t, dev.RelationToStream(stream, nil), ANYBODY, "nil parent")
 
 	dev.Enabled = false
-	if dev.RelationToStream(stream, dev) != ANYBODY {
-		t.Errorf("disabled dev")
-	}
+	assert.Equal(t, dev.RelationToStream(stream, dev), ANYBODY, "disabled dev")
 	dev.Enabled = true
 
 	dev.IsAdmin = true
-	if dev.RelationToStream(stream, dev) != ROOT {
-		t.Errorf("root dev")
-	}
+	assert.Equal(t, dev.RelationToStream(stream, dev), ROOT, "root dev")
 	dev.IsAdmin = false
 
 	dev.CanActAsUser = true
-	if dev.RelationToStream(stream, dev) != USER {
-		t.Errorf("root dev")
-	}
+	assert.Equal(t, dev.RelationToStream(stream, dev), USER, "root dev")
 	dev.CanActAsUser = false
 
 	d2 := *dev
 	d2.UserId = d2.UserId + 1
 	d2.DeviceId = d2.DeviceId + 1
-	if d2.RelationToStream(stream, dev) != ENABLED {
-		t.Errorf("different user devices got %v", dev.RelationToStream(stream, &d2))
-	}
+	assert.Equal(t, d2.RelationToStream(stream, dev), ENABLED,
+					"different user devices got %v", dev.RelationToStream(stream, &d2))
 
 	d2 = *dev
 	d2.CanActAsUser = true
 	d2.UserId += 1
 	d2.DeviceId += 1
-	if dev.RelationToStream(stream, &d2) == USER {
-		t.Errorf("d2 can be user, but isn't parent")
-	}
-
-	if dev.RelationToStream(stream, dev) != DEVICE {
-		t.Errorf("owner should be dev")
-	}
-
+	assert.NotEqual(t, dev.RelationToStream(stream, &d2), USER, "d2 can be user, but isn't parent")
+	assert.Equal(t, dev.RelationToStream(stream, dev), DEVICE, "owner should be dev")
 }
-
-
-/**
-
-
-
-func (d *Device) RelationToDevice(device *Device) (PermissionLevel)  {
-        // guards
-        if device == nil || ! d.Enabled {
-                return ANYBODY
-        }
-
-        // Permision Levels
-        if d.IsAdmin {
-                return ROOT
-        }
-
-        if d.UserId == device.UserId {
-                if d.CanActAsUser {
-                        return USER
-                }
-
-                if d.DeviceId == device.DeviceId {
-                        return DEVICE
-                }
-
-                return FAMILY
-        }
-
-
-        if d.Enabled {
-                return ENABLED
-        }
-
-        return ANYBODY
-}
-
-
-func (d *Device) RelationToStream(stream *Stream, streamParent *Device) (PermissionLevel)  {
-        // guards
-        if stream == nil || streamParent == nil || ! d.Enabled {
-                return ANYBODY
-        }
-
-        // Permision Levels
-        if d.IsAdmin {
-                return ROOT
-        }
-
-        if d.CanActAsUser && d.UserId == streamParent.UserId {
-                return USER
-        }
-
-        if d.DeviceId == stream.DeviceId {
-                return DEVICE
-        }
-
-        if d.UserId == streamParent.UserId {
-                return FAMILY
-        }
-
-        return ENABLED
-}
-
-**/
