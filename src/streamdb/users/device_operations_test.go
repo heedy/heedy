5f1df32d7097ab9a0672f10de96a4e400cf353d4
Joseph <joehms22@gmail.com>
2015-04-28 20:21:32 -0400

    updated device operations with the new testing code

--- src/streamdb/users/device_operations_test.go
+++ src/streamdb/users/device_operations_test.go
@@ -3,88 +3,56 @@ package users
 import (
 	"reflect"
 	"testing"
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/require"
 )
 
 func TestCreateDevice(t *testing.T) {
 	usr, err := CreateTestUser(testdb)
-	if err != nil {
-		t.Errorf("Cannot create test user %v", err.Error())
-		return
-	}
+	require.Nil(t, err)
 
 	usr2, err := CreateTestUser(testdb)
-	if err != nil {
-		t.Errorf("Cannot create test user %v", err.Error())
-		return
-	}
-
+	require.Nil(t, err)
 
 	err = testdb.CreateDevice("TestCreateDevice", usr.UserId)
-	if err != nil {
-		t.Errorf("Cannot create device %v", err)
-		return
-	}
+	require.Nil(t, err)
 
 	// DeviceName/Usernames must be unique
  	err = testdb.CreateDevice("TestCreateDevice", usr.UserId)
-	if err == nil {
-		t.Errorf("Created device with duplicate name under the same user")
-	}
+	assert.NotNil(t, err, "Created device with duplicate name under the same user")
 
 	// but should work with different users
 	err = testdb.CreateDevice("TestCreateDevice", usr2.UserId)
-	if err != nil {
-		t.Errorf("Could not create device with secnod user %v", err)
-		return
-	}
+	assert.Nil(t, err, "Could not create device with secnod user %v", err)
 }
 
 func TestReadDeviceById(t *testing.T) {
 	usr, err := CreateTestUser(testdb)
-	if err != nil {
-		t.Errorf("Cannot create test user %v", err.Error())
-		return
-	}
+	require.Nil(t, err)
 
 	err = testdb.CreateDevice("TestReadStreamById", usr.UserId)
-	if err != nil {
-		t.Errorf("Cannot create object %v", err)
-		return
-	}
+	require.Nil(t, err)
 
 	devforid, err := testdb.ReadDeviceForUserByName(usr.UserId, "TestReadStreamById")
-	if err != nil {
-		t.Errorf("Cannot get id %v", err)
-		return
-	}
+	require.Nil(t, err)
+
 	id := devforid.UserId
-
 	obj, err := testdb.ReadDeviceById(id)
-
-	if err != nil || obj == nil {
-		t.Errorf("Cannot read object back with returned id %v, err: %v, obj: %v", id, err, obj)
-		return
-	}
+	require.Nil(t, err)
+	require.NotNil(t, obj)
 }
 
 func TestUpdateDevice(t *testing.T) {
 	usr, err := CreateTestUser(testdb)
-	if err != nil {
-		t.Errorf("Cannot create test user %v", err.Error())
-		return
-	}
+	require.Nil(t, err)
 
 	err = testdb.CreateDevice("TestUpdateDevice", usr.UserId)
-	if err != nil {
-		t.Errorf("Cannot create object %v", err)
-		return
-	}
+	require.Nil(t, err)
 
 	obj, err := testdb.ReadDeviceForUserByName(usr.UserId, "TestUpdateDevice")
-	if err != nil || obj == nil {
-		t.Errorf("Cannot read object back with err: %v, obj:%v", err, obj)
-		return
-	}
+	require.Nil(t, err)
+	require.NotNil(t, obj)
+
 
 	obj.ApiKey = obj.ApiKey + "Testing" // should work with all UUIDs, still will be unique
 	obj.Enabled = false
@@ -92,58 +60,27 @@ func TestUpdateDevice(t *testing.T) {
 	obj.IsAdmin = true
 
 	err = testdb.UpdateDevice(obj)
-	if err != nil {
-		t.Errorf("Could not update object %v", err)
-		return
-	}
+	require.Nil(t, err)
 
 	obj2, err := testdb.ReadDeviceForUserByName(usr.UserId, "TestUpdateDevice")
-	if err != nil || obj2 == nil {
-		t.Errorf("Cannot read object back with err: {}, obj:{}", err, obj2)
-		return
-	}
+	require.Nil(t, err)
+	require.NotNil(t, obj2)
 
 	if !reflect.DeepEqual(obj, obj2) {
 		t.Errorf("The original and updated objects don't match orig: %v updated %v", obj, obj2)
 	}
 
 	err = testdb.UpdateDevice(nil)
-	if err != ERR_INVALID_PTR {
-		t.Errorf("Allowed nil pointer through")
-	}
+	assert.Equal(t, err, ERR_INVALID_PTR, "Allowed nil pointer through")
 }
 
 func TestDeleteDevice(t *testing.T) {
-	usr, err := CreateTestUser(testdb)
-	if err != nil {
-		t.Errorf("Cannot create test user %v", err.Error())
-		return
-	}
-
-	err = testdb.CreateDevice("TestDeleteDevice", usr.UserId)
-
-	if nil != err {
-		t.Errorf("Cannot create object to test delete err: %v", err)
-		return
-	}
-
-	obj, err := testdb.ReadDeviceForUserByName(usr.UserId, "TestDeleteDevice")
-	if nil != err {
-		t.Errorf("Cannot create object to test delete err: %v", err)
-		return
-	}
+	usr, obj, _, err := CreateUDS(testdb)
+	require.Nil(t, err)
 
 	err = testdb.DeleteDevice(obj.DeviceId)
-
-	if nil != err {
-		t.Errorf("Error when attempted delete %v", err)
-		return
-	}
+	assert.Nil(t, err, "error on delete %v", err)
 
 	obj, err = testdb.ReadDeviceForUserByName(usr.UserId, "TestDeleteDevice")
-
-	if err == nil {
-		t.Errorf("The object with the selected ID should have errored out, but it did not")
-		return
-	}
+	assert.NotNil(t, err, "should not succeed, device should be gone %v", err)
 }
