caa8728c22df07fde0932d7c0f25d287bc476af9
Daniel Kumor <daniel@dkumor.com>
2015-05-15 06:27:33 -0400

    REST: Initial add of stream ops (not tested)
    The stream insert and get operations need some serious testing from the python client. But other than that, the core code is finished.

--- src/plugins/rest/util.go
+++ src/plugins/rest/util.go
@@ -6,18 +6,25 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"streamdb"
 	"strings"
 
 	log "github.com/Sirupsen/logrus"
 )
 
+//Mb is nubmer of bytes in a megabyte
 const Mb = 1024 * 1024
 
 //ErrInvalidName is thrown when the name is bad
-var ErrInvalidName = errors.New("The given name did not pass sanitation.")
+var (
+	ErrInvalidName = errors.New("The given name did not pass sanitation.")
+	ErrBadQ        = errors.New("Unrecognized query command.")
+)
 
 //OK is a simplifying function that returns success
 func OK(writer http.ResponseWriter) error {
+	writer.Header().Set("Content-Length", "2")
 	writer.WriteHeader(http.StatusOK)
 	writer.Write([]byte("ok"))
 	return nil
@@ -37,6 +44,7 @@ func JSONWriter(writer http.ResponseWriter, data interface{}, logger *log.Entry,
 		logger.Errorln(err)
 		return err
 	}
+	writer.Header().Set("Content-Length", strconv.Itoa(len(res)))
 	writer.WriteHeader(http.StatusOK)
 	writer.Write(res)
 	return nil
@@ -47,7 +55,7 @@ func UnmarshalRequest(request *http.Request, unmarshalTo interface{}) error {
 	defer request.Body.Close()
 
 	//Limit requests to 10MB
-	data, err := ioutil.ReadAll(io.LimitReader(request.Body, 10 * Mb))
+	data, err := ioutil.ReadAll(io.LimitReader(request.Body, 10*Mb))
 	if err != nil {
 		return err
 	}
@@ -60,8 +68,8 @@ func ValidName(n string, err error) error {
 	if err != nil {
 		return err
 	}
-	
-	if 	strings.Contains(n, "/") ||
+
+	if strings.Contains(n, "/") ||
 		strings.Contains(n, "\\") ||
 		strings.Contains(n, " ") ||
 		strings.Contains(n, "?") ||
@@ -69,5 +77,16 @@ func ValidName(n string, err error) error {
 		return ErrInvalidName
 	}
 
+	return nil
+}
+
+//BadQ checks if there is a q= part to the given query, and gives an error if there is
+func BadQ(o streamdb.Operator, writer http.ResponseWriter, request *http.Request, arg string) error {
+	if val := request.URL.Query().Get("q"); val != "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		log.WithFields(log.Fields{"dev": o.Name(), "addr": request.RemoteAddr,
+			"op": "Q", "arg": arg}).Warningln("Bad Q: ", val)
+		return ErrBadQ
+	}
 	return nil
 }
