1ece755b51a0f8199166fb537a0debea0ba77edb
Daniel Kumor <daniel@dkumor.com>
2015-05-22 00:02:48 -0400

    Added basic windows background logger
    The code will need to be extended to linux, and will need to integrate
    with python connectordb code.
    
    This can be the beginnings of a python client for computers (gathering
    data using node is hard).

---
+++ src/collectors/computerlog/computerlog.py
@@ -0,0 +1,45 @@
+#A very simple windows keypress frequency and active window logger for connectordb
+
+import win32api
+import win32gui
+import win32console
+import pyHook,pythoncom
+import time
+
+from multiprocessing import Process,Value
+
+win=win32console.GetConsoleWindow()
+win32gui.ShowWindow(win,0)
+
+
+def NewDatapoint(keypresses,activewindow):
+    timestamp = time.time()
+    datapoint = {"t": time.time(), "d": {"keypresses":keypresses,"activewindow": activewindow}}
+    f=open("keylog.txt","r+")
+    f.write(str(datapoint)+"\n")
+    f.close()
+
+
+def runKeylogger(val):
+    def OnKeyboardEvent(event):
+        val.value +=1
+
+    hm = pyHook.HookManager()
+    hm.KeyDown = OnKeyboardEvent
+    hm.HookKeyboard()
+
+    pythoncom.PumpMessages()
+
+if __name__=="__main__":
+    num = Value('i',0)
+    p = Process(target=runKeylogger,args=(num,))
+    p.start()
+
+    try:
+        while True:
+            time.sleep(60)
+            NewDatapoint(num.value,win32gui.GetWindowText(win32gui.GetForegroundWindow()))
+            num.value=0
+    except:
+        pass
+    p.terminate()
