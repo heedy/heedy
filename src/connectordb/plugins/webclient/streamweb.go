1af5e499ba2e1f62092ee86f506da4af6890504e
Daniel Kumor <daniel@dkumor.com>
2015-06-05 21:22:29 -0500

    Made logging verbose in webclient
    Now the entire service has consistent logging.
    ERROR: server errors
    WARN: User errors
    INFO: database modifications
    DEBUG: Reads
    
    Field conventions are same:
    op: current operation - password auth failures are op="AUTH"
    addr: the address of request
    faddr: X-Forwarding-IP address
    uri: the path that was requested
    usr: the user (in webclient)
    dev: the device (in restserver)

--- src/connectordb/plugins/webclient/streamweb.go
+++ src/connectordb/plugins/webclient/streamweb.go
@@ -4,9 +4,9 @@ import (
 	"net/http"
 	"strconv"
 
-	log "github.com/Sirupsen/logrus"
-
 	"github.com/gorilla/mux"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 const (
@@ -21,20 +21,25 @@ const (
 }`
 )
 
-func readStreamPage(se *SessionEnvironment) {
+func readStreamPage(se *SessionEnvironment, logger *log.Entry) {
+	logger = logger.WithField("op", "ReadStreamPage")
+
 	pageData := make(map[string]interface{})
 	vars := mux.Vars(se.Request)
 	streamids := vars["id"]
 	streamid, _ := strconv.Atoi(streamids)
 	stream, err := se.Operator.ReadStreamByID(int64(streamid))
 	if err != nil {
-		pageData["alert"] = "Error getting stream."
-	}
-	device, err := se.Operator.ReadDeviceByID(stream.DeviceId)
-	if err != nil {
+		logger.Warn(err.Error())
 		pageData["alert"] = "Error getting stream."
 	}
 
+	device, err := se.Operator.ReadDeviceByID(stream.DeviceId)
+	if err != nil {
+		logger.Warn(err.Error())
+		pageData["alert"] = "Error getting stream."
+	}
+	logger.Debugf("Reading stream: %v/%v", device.Name, stream.Name)
 	pageData["stream"] = stream
 	pageData["user"] = se.User
 	pageData["device"] = device
@@ -43,27 +48,30 @@ func readStreamPage(se *SessionEnvironment) {
 	se.Save()
 	err = streamReadTemplate.ExecuteTemplate(se.Writer, "stream.html", pageData)
 	if err != nil {
+		logger.Error(err.Error())
 		http.Error(se.Writer, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func editStreamAction(se *SessionEnvironment) {
+func editStreamAction(se *SessionEnvironment, logger *log.Entry) {
+	logger = logger.WithField("op", "EditStreamAction")
 	vars := mux.Vars(se.Request)
 	streamids := vars["id"]
 	streamid, _ := strconv.Atoi(streamids)
 	stream, err := se.Operator.ReadStreamByID(int64(streamid))
 	if err != nil {
+		logger.Warn(err.Error())
 		se.Session.AddFlash("Error getting stream, maybe it was deleted?")
 		goto redirect
 	}
-
+	logger.Info("Update Stream: %v", stream.Name)
 	err = se.Operator.UpdateStream(stream)
 
 	if err != nil {
-		log.Errorf(err.Error())
+		logger.Warn(err.Error())
 		se.Session.AddFlash(err.Error())
 	} else {
-		se.Session.AddFlash("Created Device")
+		se.Session.AddFlash("Stream modified")
 	}
 
 redirect:
@@ -71,16 +79,18 @@ redirect:
 	http.Redirect(se.Writer, se.Request, "/secure/stream/"+streamids, http.StatusTemporaryRedirect)
 }
 
-func createStreamAction(se *SessionEnvironment) {
+func createStreamAction(se *SessionEnvironment, logger *log.Entry) {
+	logger = logger.WithField("op", "CreateStreamAction")
+
 	vars := mux.Vars(se.Request)
 	devids := vars["id"]
 	name := se.Request.PostFormValue("name")
-
+	logger.Infof("Creating: %v", name)
 	devid, _ := strconv.Atoi(devids)
 	device, err := se.Operator.ReadDeviceByID(int64(devid))
 
 	if err != nil {
-		log.Errorf(err.Error())
+		logger.Warn(err.Error())
 		se.Session.AddFlash("Error getting device, maybe it was deleted?")
 		goto redirect
 	}
@@ -88,7 +98,7 @@ func createStreamAction(se *SessionEnvironment) {
 	err = se.Operator.CreateStreamByDeviceID(device.DeviceId, name, defaultTemplate)
 
 	if err != nil {
-		log.Errorf(err.Error())
+		logger.Warn(err.Error())
 		se.Session.AddFlash("Error creating stream.")
 	}
 
--- src/connectordb/plugins/webclient/userweb.go
+++ src/connectordb/plugins/webclient/userweb.go
@@ -6,12 +6,11 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
-func getUserPage(se *SessionEnvironment) {
-	log.Printf("getting user page")
+func getUserPage(se *SessionEnvironment, logger *log.Entry) {
+	logger = logger.WithField("op", "GetUserPage")
+	logger.Debugln()
 
 	pageData := make(map[string]interface{})
-
-	log.Debugf("userdb: %p, user: %v", userdb, se.User)
 
 	devices, err := userdb.ReadAllDevicesByUserID(se.User.UserId)
 	pageData["devices"] = devices
@@ -19,40 +18,46 @@ func getUserPage(se *SessionEnvironment) {
 	pageData["flashes"] = se.Session.Flashes()
 
 	if err != nil {
+		logger.Warn(err.Error())
 		pageData["alert"] = "Error getting devices."
 	}
 	se.Save()
 
 	err = loginHomeTemplate.ExecuteTemplate(se.Writer, "root.html", pageData)
 	if err != nil {
+		logger.Error(err.Error())
 		http.Error(se.Writer, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func editUserPage(se *SessionEnvironment) {
+func editUserPage(se *SessionEnvironment, logger *log.Entry) {
+	logger = logger.WithField("op", "EditUserPage")
+	logger.Debugln()
 	pageData := make(map[string]interface{})
 
-	log.Printf("Editing %v", se.User.Name)
 	devices, err := userdb.ReadAllDevicesByUserID(se.User.UserId)
 	pageData["devices"] = devices
 	pageData["user"] = se.User
 	pageData["flashes"] = se.Session.Flashes()
 
 	if err != nil {
+		logger.Warn(err.Error())
 		pageData["alert"] = "Error getting devices."
 	}
 
 	se.Save()
 	err = userEditTemplate.ExecuteTemplate(se.Writer, "user_edit.html", pageData)
 	if err != nil {
+		logger.Error(err.Error())
 		http.Error(se.Writer, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func modifyUserAction(se *SessionEnvironment) {
+func modifyUserAction(se *SessionEnvironment, logger *log.Entry) {
+	logger = logger.WithField("op", "ModifyEmailAction")
 	email := se.Request.PostFormValue("email")
 
-	log.Printf("Modifying user %v, new email: %v", se.User.Name, email)
+	logger.Infof("new email: %v", email)
 
 	// TODO someday change this to send a link to the user's email address
 	// and when they click on it change the email (send them their email in the
@@ -60,10 +65,11 @@ func modifyUserAction(se *SessionEnvironment) {
 	if email != "" {
 		se.User.Email = email
 
-		log.Printf("email passed first check")
+		logger.Debugf("email passed first check")
 		err := se.Operator.UpdateUser(se.User)
 
 		if err != nil {
+			logger.Warn(err.Error())
 			se.Session.AddFlash(err.Error())
 		} else {
 			se.Session.AddFlash("Settings Updated")
@@ -73,24 +79,27 @@ func modifyUserAction(se *SessionEnvironment) {
 	http.Redirect(se.Writer, se.Request, "/secure/edit", http.StatusTemporaryRedirect)
 }
 
-func modifyPasswordAction(se *SessionEnvironment) {
+func modifyPasswordAction(se *SessionEnvironment, logger *log.Entry) {
+	logger = logger.WithField("op", "ModifyPasswordAction")
 
 	p0 := se.Request.PostFormValue("current_password")
 	p1 := se.Request.PostFormValue("password1")
 	p2 := se.Request.PostFormValue("password2")
 
-	log.Printf("Modifying user %v, new password: %v", se.User.Name, p1)
+	logger.Info() //Do not display password - just log that modification was requested
 
 	if p1 == p2 && p1 != "" && se.User.ValidatePassword(p0) {
 		se.User.SetNewPassword(p1)
 		err := se.Operator.UpdateUser(se.User)
 
 		if err != nil {
+			logger.Warn(err.Error())
 			se.Session.AddFlash(err.Error())
 		} else {
 			se.Session.AddFlash("Your password has been updated.")
 		}
 	} else {
+		logger.Warn("Password Mismatch")
 		se.Session.AddFlash("Your passwords did not match, try again.")
 	}
 
@@ -98,14 +107,16 @@ func modifyPasswordAction(se *SessionEnvironment) {
 	http.Redirect(se.Writer, se.Request, "/secure/edit", http.StatusTemporaryRedirect)
 }
 
-func deleteUserAction(se *SessionEnvironment) {
+func deleteUserAction(se *SessionEnvironment, logger *log.Entry) {
+	logger = logger.WithField("op", "DeleteUserAction")
 	p0 := se.Request.PostFormValue("password")
-	log.Printf("Deleting user %v", se.User.Name)
+	logger.Info()
 
 	if se.User.ValidatePassword(p0) {
 		err := se.Operator.DeleteUserByID(se.User.UserId)
 
 		if err != nil {
+			logger.Warn(err.Error())
 			se.Session.AddFlash(err.Error())
 		} else {
 			se.Session.AddFlash("Your password has been updated.")
@@ -122,6 +133,7 @@ func newUserPage(writer http.ResponseWriter, r *http.Request) {
 
 	err := addUserTemplate.ExecuteTemplate(writer, "newuser.html", pageData)
 	if err != nil {
+		getLogger(r).Errorln(err.Error())
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
 	}
 }
