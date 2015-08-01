64839e74585e1c439931d4602fff7c36e914ee6d
Daniel Kumor <daniel@dkumor.com>
2015-08-01 02:09:20 -0500

    Fixed closing bug
    connectordb was not shutting down on ctrl+c - it needed kill -9. Now after all close functions are run, os.Exit is called

--- src/connectordb/streamdb/util/close_on_exit.go
+++ src/connectordb/streamdb/util/close_on_exit.go
@@ -3,7 +3,18 @@ package util
 import (
 	"os"
 	"os/signal"
+	"sync"
+	"sync/atomic"
 	"syscall"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+var (
+	//The number of goroutines waiting for a close signal
+	closeNumber  = int32(0)
+	closeCounter = make(chan bool, 1)
+	closeExiter  = sync.Once{}
 )
 
 type Closeable interface {
@@ -12,6 +23,21 @@ type Closeable interface {
 
 // CloseOnExit closes a resource when the program is exiting.
 func CloseOnExit(closeable Closeable) {
+	log.Debug("ADDEXIT")
+	atomic.AddInt32(&closeNumber, 1)
+	closeExiter.Do(func() {
+		go func() {
+			cnum := atomic.LoadInt32(&closeNumber)
+			for cnum > 0 {
+				<-closeCounter
+				log.Debug("Got an exit")
+				cnum = atomic.LoadInt32(&closeNumber)
+			}
+			log.Warn("Exiting...")
+			os.Exit(0)
+		}()
+	})
+
 	if closeable == nil {
 		return
 	}
@@ -20,8 +46,17 @@ func CloseOnExit(closeable Closeable) {
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				atomic.AddInt32(&closeNumber, -1)
+				closeCounter <- false
+			}
+		}()
 		<-c
+		log.Debug("EXIT")
 		closeable.Close()
+		atomic.AddInt32(&closeNumber, -1)
+		closeCounter <- true
 	}()
 }
 
