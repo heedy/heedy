83d4629f17f44d47e2bc17afb91a7c3f47b0bd54
Daniel Kumor <daniel@dkumor.com>
2015-08-03 03:51:51 -0500

    Transform Pipeline
    The transform pipeline is functional - the dataset backend needs to integrate it with data dataranges, but it correctly fiters/transforms data now.
    
    The function NewTransformPipeline takes a pipeline string and implements the full pipeline as a single transform.

---
+++ src/connectordb/streamdb/dataset/transforms/pipeline.go
@@ -0,0 +1,81 @@
+package transforms
+
+import (
+	"connectordb/streamdb/dataset/pipeline"
+	. "connectordb/streamdb/datastream"
+	"errors"
+)
+
+type TransformPipeline struct {
+	transforms []DatapointTransform
+}
+
+func (p *TransformPipeline) Transform(dp *Datapoint) (rdp *Datapoint, err error) {
+	if len(p.transforms) == 0 {
+		return dp, nil
+	}
+	if dp != nil {
+		//go through the transforms one by one. If one transform returns nil, short-circuit
+		//returning nil, asking for another datapoint
+		for i := 0; i < len(p.transforms); i++ {
+			dp, err = p.transforms[i].Transform(dp)
+			if err != nil || dp == nil {
+				return dp, err
+			}
+		}
+	} else {
+		//the datapoint inputted was nil - that means that the DataRange is done,
+		//and it is asking for any cached datapoints. clear the transforms that pass
+		//nil, and loop through the ones that don't return nil until a datapoint gets
+		//through the full transform, or the full array is cleared
+
+		for dp == nil && len(p.transforms) > 0 {
+
+			//get rid of starting nils
+			for i := 0; i < len(p.transforms); i++ {
+				dp, err = p.transforms[i].Transform(dp)
+				if err != nil {
+					return dp, err
+				}
+				if dp != nil {
+					p.transforms = p.transforms[i:]
+				}
+			}
+
+			//Try finishing the pipeline
+			for i := 1; i < len(p.transforms); i++ {
+				dp, err = p.transforms[i].Transform(dp)
+				if err != nil {
+					return dp, err
+				}
+				if dp == nil {
+					break //damn, need to try again - the value got filtered
+				}
+			}
+		}
+	}
+	return dp, err
+}
+
+func NewTransformPipeline(pipe string) (*TransformPipeline, error) {
+	p, err := pipeline.ParsePipeline(pipe)
+	if err != nil {
+		return nil, err
+	}
+
+	transforms := make([]DatapointTransform, 0)
+
+	for i := 0; i < len(p); i++ {
+		tfnc, ok := Transforms[p[i].Symbol]
+		if !ok {
+			return nil, errors.New("Could not find '" + p[i].Symbol + "' transform.")
+		}
+		t, err := tfnc(p[i].Args)
+		if err != nil {
+			return nil, err
+		}
+		transforms = append(transforms, t)
+	}
+
+	return &TransformPipeline{transforms}, nil
+}
---
+++ src/connectordb/streamdb/dataset/transforms/pipeline_test.go
@@ -0,0 +1,48 @@
+package transforms
+
+import (
+	. "connectordb/streamdb/datastream"
+	"testing"
+
+	"github.com/connectordb/duck"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPipeline(t *testing.T) {
+	testcases := []struct {
+		Pipeline  string
+		Haserror  bool
+		Haserror2 bool
+		Input     *Datapoint
+		Output    *Datapoint
+	}{
+		{"lt(5)", false, false, &Datapoint{Data: 4}, &Datapoint{Data: true}},
+		{"has('test'):lt(1)", false, false, &Datapoint{Data: 4}, &Datapoint{Data: true}},
+		{"ifhas('test'):lt(1)", false, false, &Datapoint{Data: 4}, nil},
+		{"ifhas('test'):get('test'):lt(1)", false, false, &Datapoint{Data: map[string]interface{}{"test": 25}}, &Datapoint{Data: false}},
+		{"ifhas('tst'):get('test'):lt(1)", false, false, &Datapoint{Data: map[string]interface{}{"test": 25}}, nil},
+		{"ifhas('test'):get('test'):gt(1)", false, false, &Datapoint{Data: map[string]interface{}{"test": 25}}, &Datapoint{Data: true}},
+		{"ifhas('test", true, false, nil, nil},
+		{"get('test')", false, true, &Datapoint{Data: 4}, nil},
+	}
+
+	for _, c := range testcases {
+		tr, err := NewTransformPipeline(c.Pipeline)
+		if c.Haserror {
+			require.Error(t, err, duck.JSONString(c))
+		} else {
+			require.NoError(t, err, duck.JSONString(c))
+			dp, err := tr.Transform(c.Input)
+			if c.Haserror2 {
+				require.Error(t, err, duck.JSONString(c))
+			} else {
+				require.NoError(t, err, duck.JSONString(c))
+				if c.Output != nil {
+					require.Equal(t, c.Output.String(), dp.String(), duck.JSONString(c))
+				} else {
+					require.Nil(t, dp, duck.JSONString(c))
+				}
+			}
+		}
+	}
+}
