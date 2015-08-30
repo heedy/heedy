691c258746115399963e95e661453ff34ea762b6
Joseph <joehms22@gmail.com>
2015-08-29 21:40:24 -0400

    removed unused file, fixed division's name in algebraic operators

--- src/connectordb/streamdb/query/transforms/algebraic_operators.go
+++ src/connectordb/streamdb/query/transforms/algebraic_operators.go
@@ -18,7 +18,7 @@ func multiplyTransformGenerator(left TransformFunc, right TransformFunc) Transfo
 
 // Divides the left and right hand side
 func divideTransformGenerator(left TransformFunc, right TransformFunc) TransformFunc {
-	return binaryOperatorValueWrapper("*", left, right, func(leftVal, rightVal float64) (float64, error) {
+	return binaryOperatorValueWrapper("/", left, right, func(leftVal, rightVal float64) (float64, error) {
 		if rightVal == 0.0 || rightVal == -0.0 {
 			return 0, errors.New("Cannot divide by zero")
 		}
