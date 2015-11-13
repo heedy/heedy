a7a93734de92da7087e9ff3058f7090443f74188
Daniel Kumor <daniel@dkumor.com>
2015-11-13 15:51:59 -0600

    Login from web is functional
    To logout, all that is needed is an xmlhttprequest to /api/v1/logout with cookies.

---
+++ site/www/js/main.js
@@ -0,0 +1,44 @@
+
+//login attempts to log into ConnectorDB. If successful, it refreshes the site. if not, it notifies the user.
+function login() {
+	usrname = $("#username").val()
+	pass = $("#password").val()
+
+	if (usrname == "") {
+		alert("Please type in a username!");
+	} else if (pass=="") {
+		alert("Please type in a password!");
+	} else {
+		//While waiting for response, disable the inputs
+		$("#username").prop('disabled', true);
+		$("#password").prop('disabled', true);
+
+		//Set the auth header
+		authHeader = "Basic " + btoa(usrname + ":" + pass);
+
+		$.ajax({
+			type: "GET",
+			xhrFields: {
+				withCredentials: true
+			},
+			url: "/api/v1/login",
+			success: function(data) {
+					location.reload(true);
+			},
+			error: function(request, textStatus, errorThrown) {
+				$("#msform").addClass("has-error")
+				$("#username").prop('disabled', false);
+				$("#password").prop('disabled', false);
+				$("#password").val("");
+				$("#password").focus();
+			},
+			beforeSend: function (xhr) {
+		        xhr.setRequestHeader('Authorization', 'Basic ' + btoa(usrname + ":" + pass));
+		    }
+		});
+
+
+	}
+
+	return false;
+}
