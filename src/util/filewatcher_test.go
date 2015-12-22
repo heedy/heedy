da5b812586afebfe86893e9e5144711ee93ad429
Daniel Kumor <daniel@dkumor.com>
2015-12-22 11:50:49 -0600

    MORE time increase
    Travis keeps failing...

--- src/util/filewatcher_test.go
+++ src/util/filewatcher_test.go
@@ -27,7 +27,7 @@ func TestFileWatcher(t *testing.T) {
 
 	tr := TestReloader{}
 
-	time.Sleep(50 * time.Millisecond)
+	time.Sleep(200 * time.Millisecond)
 
 	_, err := NewFileWatcher("testfile.txt", &tr)
 	require.NoError(t, err)
@@ -36,13 +36,13 @@ func TestFileWatcher(t *testing.T) {
 
 	// Modification of file should call reload
 	ioutil.WriteFile("testfile.txt", []byte("test"), 0755)
-	time.Sleep(50 * time.Millisecond)
+	time.Sleep(200 * time.Millisecond)
 	require.Equal(t, 1, tr.callnum)
 
 	// Deleting a file should do nothing, but should call reload once it
 	// is recreated
 	require.NoError(t, os.Remove("testfile.txt"))
-	time.Sleep(50 * time.Millisecond)
+	time.Sleep(200 * time.Millisecond)
 	require.Equal(t, 1, tr.callnum)
 
 	ioutil.WriteFile("testfile.txt", []byte("test2"), 0755)
@@ -52,7 +52,7 @@ func TestFileWatcher(t *testing.T) {
 	// Now renaming a file, and then modifying it should do nothing
 	os.Rename("testfile.txt", "testfile2.txt")
 	ioutil.WriteFile("testfile2.txt", []byte("test3"), 0755)
-	time.Sleep(50 * time.Millisecond)
+	time.Sleep(200 * time.Millisecond)
 	require.Equal(t, 2, tr.callnum)
 
 	// And lastly, renaming a file back to the original name should be caught
