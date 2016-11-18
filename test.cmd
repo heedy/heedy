33578e47159a7262e4c6282b5bfc286111a95a4c
Daniel R. Kumor <daniel@dkumor.com>
2016-11-18 18:58:57 -0500

    Added test flags to appveyor

--- test.cmd
+++ test.cmd
@@ -1,4 +1,4 @@
-bin\connectordb.exe -l=DEBUG create testdb --sqlbackend=sqlite3
+bin\connectordb.exe -l=DEBUG create testdb --test --sqlbackend=sqlite3
 bin\connectordb.exe start testdb
 nosetests connectordb-python/connectordb_test.py
 nosetests connectordb-python/query_test.py
