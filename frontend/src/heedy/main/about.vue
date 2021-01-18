068940f6316337173f5f613d7397dbf7e1cb9289
Daniel R. Kumor <daniel@dkumor.com>
2021-01-17 21:46:44 -0500

    Added file missing from previous commit

---
+++ frontend/src/heedy/main/about.vue
@@ -0,0 +1,46 @@
+<template >
+  <h-page-container>
+    <v-card>
+      <v-card-text class="text--primary">
+        <v-container>
+          <v-layout row justify-center>
+            <v-flex text-center style="padding-top: 1cm">
+              <h1 style="color: #1976d2">heedy</h1>
+              <h4>Live an examined life.</h4>
+            </v-flex>
+          </v-layout>
+          <v-layout row justify-center>
+            <v-flex
+              text-center
+              xs12
+              md8
+              lg6
+              style="padding-top: 1cm; backgroud-color: blue"
+            >
+              <p>
+                Heedy is an open-source, extensible home for your
+                quantified-self data, and other information your devices gather
+                about you. It enables you to easily analyze and share things you
+                find in your data, and has a powerful plugin architecture
+                allowing integration with the most popular data gathering
+                services.
+              </p>
+            </v-flex>
+          </v-layout>
+          <v-layout row justify-center>
+            <v-flex text-center style="padding-top: 1cm; backgroud-color: blue">
+              <h5 style="padding-bottom: 1cm">
+                For more information visit
+                <a hfre="https://heedy.org">heedy.org</a>
+              </h5>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+    </v-card>
+  </h-page-container>
+</template>
+
+<script>
+export default {};
+</script>
